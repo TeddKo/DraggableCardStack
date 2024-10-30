d9f24b2ccde1f0397c72f1e1e3d1949de5b5a899
mh.ko <tedd9415@nm-metaworld.com>
2024-10-30 11:45:15 +0900

    initialize 1.0.0

--- demo/feature/src/main/kotlin/tddy/ko/cardstack/feature/MainActivity.kt
+++ demo/feature/src/main/kotlin/tddy/ko/cardstack/feature/MainActivity.kt
@@ -4,22 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
 import coil3.compose.AsyncImage
@@ -45,28 +44,34 @@ class MainActivity : ComponentActivity() {
                 ) {
                     DraggableCardStack(
                         initialItems = images,
-                        height = 300.dp,
+                        height = 200.dp,
                         cardSpacingRatio = .1f,
                         cardAlignment = CardAlignment.BOTTOM,
                         dragAlignment = DragAlignment.NONE
                     ) {
-                        Box(
-                            modifier = Modifier
-                                .clip(shape = RoundedCornerShape(10.dp))
+                        Card(
+                            modifier = Modifier.fillMaxSize()
                         ) {
-                            AsyncImage(
-                                modifier = Modifier.fillMaxSize(),
-                                model = it.asset,
-                                contentDescription = it.description,
-                                contentScale = ContentScale.Crop
-                            )
-                            Text(
+                            Row(
                                 modifier = Modifier
-                                    .align(alignment = Alignment.BottomEnd)
-                                    .padding(bottom = 10.dp, end = 10.dp),
-                                text = it.description,
-                                color = Color.White
-                            )
+                                    .fillMaxSize()
+                                    .padding(16.dp),
+                                horizontalArrangement = Arrangement.spacedBy(space = 16.dp),
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                AsyncImage(
+                                    modifier = Modifier
+                                        .size(100.dp)
+                                        .clip(shape = CircleShape),
+                                    model = it.asset,
+                                    contentDescription = it.description,
+                                    contentScale = ContentScale.Crop,
+                                )
+                                Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+                                    Text(text = it.description)
+                                    Text(text = it.description)
+                                }
+                            }
                         }
                     }
                 }
